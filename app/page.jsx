fb48ac927e5c1395d8d398ffbc1b28ecc6c53bf0
Geomty <69478549+Geomty@users.noreply.github.com>
2025-07-01 13:45:47 -0400

    create flow

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,47 @@
+"use client";
+
+import { useState, useCallback } from "react";
+import { ReactFlow, applyNodeChanges, applyEdgeChanges, addEdge } from "@xyflow/react";
+import "@xyflow/react/dist/style.css";
+
+const initialNodes = [
+  {
+    id: "1",
+    position: { x: 0, y: 0 },
+    data: { label: "Hello" }
+  },
+  {
+    id: "2",
+    position: { x: 0, y: 100 },
+    data: { label: "World" }
+  }
+];
+
+const initialEdges = [
+  {
+    id: "1-2",
+    source: "1",
+    target: "2"
+  }
+];
+
 export default function Home() {
+  const [nodes, setNodes] = useState(initialNodes);
+  const [edges, setEdges] = useState(initialEdges);
+  const onNodesChange = useCallback(changes => setNodes(nds => applyNodeChanges(changes, nds)), []);
+  const onEdgesChange = useCallback(changes => setEdges(eds => applyEdgeChanges(changes, eds)), []);
+  const onConnect = useCallback(params => setEdges(eds => addEdge(params, eds)), []);
+
   return (
-    <div className="text-8xl">hello!</div>
+    <div className="w-screen h-screen">
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        proOptions={{ hideAttribution: true }}
+      />
+    </div>
   );
 }
