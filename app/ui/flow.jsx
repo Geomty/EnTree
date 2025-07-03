f7518a014a93837b6ec46725832196b558e169db
Geomty <69478549+Geomty@users.noreply.github.com>
2025-07-03 16:58:24 -0400

    node zoom in

--- app/ui/flow.jsx
+++ app/ui/flow.jsx
@@ -4,6 +4,7 @@ import { useState, useCallback, useEffect } from "react";
 import { ReactFlow, applyNodeChanges, applyEdgeChanges, addEdge } from "@xyflow/react";
 import Node from "@/app/ui/node";
 import Edge from "@/app/ui/edge";
+import { ActiveContext } from "@/app/lib/context";
 
 const nodeTypes = { node: Node };
 const edgeTypes = { edge: Edge };
@@ -33,6 +34,7 @@ const initialEdges = [
 ];
 
 export default function Flow() {
+  const active = useState(null);
   const [nodes, setNodes] = useState(initialNodes);
   const [edges, setEdges] = useState(initialEdges);
   const onNodesChange = useCallback(changes => setNodes(nds => applyNodeChanges(changes, nds)), []);
@@ -49,30 +51,38 @@ export default function Flow() {
   }
 
   return (
-    <div className="w-screen h-screen bg-white dark:bg-neutral-950">
-      <ReactFlow
-        nodes={nodes}
-        edges={edges}
-        nodeTypes={nodeTypes}
-        edgeTypes={edgeTypes}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        // onConnect={onConnect}
-        proOptions={{ hideAttribution: true }}
-        fitView={true}
-        nodesConnectable={false}
-        nodesFocusable={false}
-        edgesFocusable={false}
-        elementsSelectable={false}
-        selectNodesOnDrag={false}
-        autoPanSpeed={5}
-        zoomOnDoubleClick={false}
-        deleteKeyCode={null}
-        selectionKeyCode={null}
-        multiSelectionKeyCode={null}
-        zoomActivationKeyCode={null}
-        panActivationKeyCode={null}
-      />
-    </div>
+    <ActiveContext value={active}>
+      <div className={(active[0] && "brightness-50 ") + "w-screen h-screen bg-white dark:bg-neutral-950"}>
+        <ReactFlow
+          nodes={nodes}
+          edges={edges}
+          nodeTypes={nodeTypes}
+          edgeTypes={edgeTypes}
+          onNodesChange={onNodesChange}
+          onEdgesChange={onEdgesChange}
+          // onConnect={onConnect}
+          proOptions={{ hideAttribution: true }}
+          fitView={true}
+          fitViewOptions={{ padding: 2 }}
+          maxZoom={10}
+          panOnDrag={!active[0]}
+          zoomOnScroll={!active[0]}
+          zoomOnPinch={!active[0]}
+          // nodesDraggable={!active[0]}
+          nodesConnectable={false}
+          nodesFocusable={false}
+          edgesFocusable={false}
+          elementsSelectable={false}
+          selectNodesOnDrag={false}
+          autoPanSpeed={5}
+          zoomOnDoubleClick={false}
+          deleteKeyCode={null}
+          selectionKeyCode={null}
+          multiSelectionKeyCode={null}
+          zoomActivationKeyCode={null}
+          panActivationKeyCode={null}
+        />
+      </div>
+    </ActiveContext>
   );
 }
--- app/ui/node.jsx
+++ app/ui/node.jsx
@@ -1,13 +1,25 @@
+import { useCallback, useContext } from "react";
 import { Handle as FlowHandle, useReactFlow } from "@xyflow/react";
+import { ActiveContext } from "@/app/lib/context";
 
 export default function Node(props) {
   const reactFlow = useReactFlow();
+  const active = useContext(ActiveContext);
+  const onClick = useCallback(() => {
+    reactFlow.fitView({ duration: 1000, nodes: [{ id: props.id }] });
+    setTimeout(() => {
+      if (active[0] == props.id) {
+        active[1](null);
+      } else if (active[0] == null) {
+        active[1](props.id);
+      }
+    }, 1); // optimal solution!
+    reactFlow.updateNodeData(props.id, { complete: props.data.complete ? false : true });
+  }, [active]);
 
   return (
     <div className={(props.data.complete ? "opacity-50" : "opacity-100") + " px-6 py-3 bg-neutral-100 border border-black dark:bg-neutral-800 dark:border-neutral-500 rounded-lg"}>
-      <p className="text-md text-black dark:text-neutral-200 hover:cursor-pointer" onClick={() => {
-        reactFlow.updateNodeData(props.id, { complete: props.data.complete ? false : true });
-      }}>{props.data.label}</p>
+      <p className="text-md text-black dark:text-neutral-200 hover:cursor-pointer" onClick={onClick}>{props.data.label}</p>
       <Handle type="source" position="bottom" />
       <Handle type="target" position="top" />
     </div>
