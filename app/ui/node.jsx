ead576d41060f186550ac4125cc7f0bfd1383829
Geomty <69478549+Geomty@users.noreply.github.com>
2025-07-04 12:26:55 -0400

    fix selecting text

--- app/ui/node.jsx
+++ app/ui/node.jsx
@@ -17,11 +17,11 @@ export default function Node(props) {
   }, [active]);
 
   return (
-    <div className={(props.data.complete ? "opacity-50 " : "opacity-100 ") + (active[0] ? "cursor-auto " : "") + "w-96 h-52 flex justify-center content-center text-center bg-neutral-100 border-4 border-black dark:bg-neutral-800 dark:border-neutral-500 rounded-4xl"}>
+    <div className={(props.data.complete ? "opacity-50 " : "opacity-100 ") + (active[0]?.id == props.id ? "cursor-auto " : "") + "w-96 h-52 flex justify-center content-center text-center bg-neutral-100 border-4 border-black dark:bg-neutral-800 dark:border-neutral-500 rounded-4xl"}>
       {active[0]?.id == props.id ?
-        <div className="m-4 w-full flex flex-col justify-between content-center select-all">
-          <p className="text-xl">{props.data.title}</p>
-          <p className="text-sm">{props.data.description}</p>
+        <div className="m-4 w-full flex flex-col justify-between content-center nodrag">
+          <p className="text-xl select-text">{props.data.title}</p>
+          <p className="text-sm select-text">{props.data.description}</p>
           <div className="flex justify-end content-center">
             <button type="button" className="px-[0.4rem] py-[0.2rem] text-[0.6rem] bg-neutral-300 border border-black dark:bg-neutral-700 dark:border-neutral-500 rounded-md hover:cursor-pointer" onClick={onClick}>Mark as complete</button>
           </div>
