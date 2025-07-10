d015cfe69b718d920cd17d0a68c69932d98951cb
Geomty <69478549+Geomty@users.noreply.github.com>
2025-07-10 11:37:03 -0400

    basic form ui

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,14 @@
 import Flow from "@/app/ui/flow";
+import FlowForm from "@/app/ui/flow-form";
 import ThemeToggle from "@/app/ui/theme-toggle";
 
 export default function Home() {
   return (
     <div className="text-black dark:text-neutral-200">
-      <ThemeToggle />
+      <div className="absolute top-5 left-5 z-10 flex justify-start items-center gap-8">
+        <ThemeToggle />
+        <FlowForm />
+      </div>
       <Flow />
     </div>
   )
---
+++ app/ui/flow-form.jsx
@@ -0,0 +1,8 @@
+export default function FlowForm() {
+  return (
+    <form className="flex justify-start items-center gap-4">
+      <input type="text" name="query" placeholder="Enter anything" title="Enter anything" required className="h-9 pl-2 bg-neutral-100 border-2 border-black dark:bg-neutral-800 dark:border-neutral-500 rounded-lg" />
+      <button type="submit" className="px-3 py-1 bg-neutral-100 border-2 border-black dark:bg-neutral-800 dark:border-neutral-500 rounded-lg hover:cursor-pointer">Submit</button>
+    </form>
+  )
+}
