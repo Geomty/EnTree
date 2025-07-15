5d08a351c176035b30d49d8d220d5d4060e99036
Geomty <69478549+Geomty@users.noreply.github.com>
2025-07-15 17:47:52 -0400

    gemini api

--- app/lib/actions.js
+++ app/lib/actions.js
@@ -1,14 +1,28 @@
 "use server";
 
+import { GoogleGenAI } from "@google/genai";
+
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+
 export async function createChildren(prevState, formData) {
-  return [
-    {
-      title: formData.get("query") + "1",
-      description: "This is another description."
-    },
-    {
-      title: formData.get("query") + "2",
-      description: "This is yet another description."
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: JSON.stringify([
+      {
+        title: formData.get("query") + "1",
+        description: "This is another description."
+      },
+      {
+        title: formData.get("query") + "2",
+        description: "This is yet another description."
+      }
+    ]),
+    config: {
+      systemInstruction: "Repeat exactly what is inputted.",
+      thinkingConfig: {
+        thinkingBudget: 0
+      }
     }
-  ];
+  });
+  return JSON.parse(response.text);
 }
