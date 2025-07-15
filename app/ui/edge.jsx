036b4942dafbdb38a93985fff67dea0df639595b
Geomty <69478549+Geomty@users.noreply.github.com>
2025-07-15 15:41:25 -0400

    change edge type

--- app/ui/edge.jsx
+++ app/ui/edge.jsx
@@ -1,7 +1,7 @@
-import { BaseEdge, getSmoothStepPath } from "@xyflow/react";
+import { BaseEdge, getBezierPath } from "@xyflow/react";
 
 export default function Edge({ id, sourceX, sourceY, targetX, targetY }) {
-  const [path] = getSmoothStepPath({ sourceX, sourceY, targetX, targetY, borderRadius: 40, offset: 40 });
+  const [path] = getBezierPath({ sourceX, sourceY, targetX, targetY, borderRadius: 40, offset: 40 });
 
   return <BaseEdge id={id} path={path} className="!stroke-4 !stroke-neutral-500" />
 }
