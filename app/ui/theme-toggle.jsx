6882cb2c4ef215bea8006182c024378f1c0b2c85
Geomty <69478549+Geomty@users.noreply.github.com>
2025-07-29 16:50:30 -0400

    theme toggle switch

--- app/ui/theme-toggle.jsx
+++ app/ui/theme-toggle.jsx
@@ -1,19 +1,15 @@
 import { useTheme } from "next-themes";
-import { montserrat } from "@/app/lib/fonts";
+import { motion } from "motion/react";
 
 export default function ThemeToggle({ formStyle }) {
   const { resolvedTheme, setTheme } = useTheme();
 
-  return <button
-    type="button"
-    title="Theme"
-    onClick={() => {
-      if (resolvedTheme == "light") {
-        setTheme("dark");
-      } else if (resolvedTheme == "dark") {
-        setTheme("light");
-      }
-    }}
-    className={"px-3 py-1 rounded-lg hover:cursor-pointer font-black " + montserrat.className + " " + formStyle}
-  >Theme</button>
+  return (
+    <div onClick={() => {
+      if (resolvedTheme == "light") setTheme("dark");
+      else if (resolvedTheme == "dark") setTheme("light");
+    }} className={"w-18 h-10 p-1 flex items-center !rounded-full hover:cursor-pointer " + formStyle + (resolvedTheme == "light" ? " justify-start" : " justify-end")}>
+      <motion.div layout transition={{ type: "tween", duration: 0.2, ease: "easeInOut" }} className="size-7 bg-neutral-200 border border-black dark:bg-neutral-700 dark:border-neutral-500 rounded-full"></motion.div>
+    </div>
+  )
 }
