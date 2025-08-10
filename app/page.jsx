0e9c7c8ab1f9c66385ae0e405fcba6676a965283
Geomty <69478549+Geomty@users.noreply.github.com>
2025-08-10 13:43:12 -0400

    some front page layout

--- app/page.jsx
+++ app/page.jsx
@@ -1,19 +1,23 @@
 import { FcGoogle } from "react-icons/fc";
-import { roboto } from "@/app/lib/fonts";
+import { montserrat, roboto } from "@/app/lib/fonts";
 import { signIn } from "@/auth";
 
 export default function Home() {
   return (
-    <form
-      action={async () => {
-        "use server";
-        await signIn("google", { redirectTo: "/tree" });
-      }}
-    >
-      <button type="submit" className="border-neutral-500 px-3 py-2 flex items-center gap-3 bg-white border rounded-full hover:cursor-pointer hover:bg-neutral-200 !transition-all !duration-200 !ease-in-out">
-        <FcGoogle className="size-6" />
-        <p className={"text-neutral-700 text-md font-medium " + roboto.className}>Sign in with Google</p>
-      </button>
-    </form>
+    <div className="absolute top-1/2 lg:left-1/4 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col lg:items-start items-center gap-10">
+      <h1 className={"text-black lg:text-8xl text-7xl font-black " + montserrat.className}>EnTree</h1>
+      <p className="text-neutral-700 lg:text-2xl text-xl lg:text-left text-center">Your entry into AI-guided learning.<br></br>Generate a visual learning plan about any topic.</p>
+      <form
+        action={async () => {
+          "use server";
+          await signIn("google", { redirectTo: "/tree" });
+        }}
+      >
+        <button type="submit" className="border-neutral-500 px-3 py-2 flex items-center gap-3 bg-white border rounded-full hover:cursor-pointer hover:bg-neutral-200 !transition-all !duration-200 !ease-in-out">
+          <FcGoogle className="size-6" />
+          <p className={"text-neutral-700 text-md font-medium " + roboto.className}>Sign in with Google</p>
+        </button>
+      </form>
+    </div>
   )
 }
