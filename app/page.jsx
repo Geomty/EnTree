ecbe6bd3a4d708217441585752e49a7d98d1231b
Geomty <69478549+Geomty@users.noreply.github.com>
2025-08-10 13:32:36 -0400

    fix sign in button styling

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,6 @@
 import { FcGoogle } from "react-icons/fc";
 import { roboto } from "@/app/lib/fonts";
 import { signIn } from "@/auth";
-import { google } from "@/app/global.css";
 
 export default function Home() {
   return (
@@ -11,7 +10,7 @@ export default function Home() {
         await signIn("google", { redirectTo: "/tree" });
       }}
     >
-      <button id={google} type="submit" className="px-3 py-2 flex items-center gap-3 bg-white border border-neutral-500 rounded-full hover:cursor-pointer hover:bg-neutral-200 !transition-all !duration-200 !ease-in-out">
+      <button type="submit" className="border-neutral-500 px-3 py-2 flex items-center gap-3 bg-white border rounded-full hover:cursor-pointer hover:bg-neutral-200 !transition-all !duration-200 !ease-in-out">
         <FcGoogle className="size-6" />
         <p className={"text-neutral-700 text-md font-medium " + roboto.className}>Sign in with Google</p>
       </button>
