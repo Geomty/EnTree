3fdafd56c24ad2776a87a5791a3bcd0e0607ec8c
Geomty <69478549+Geomty@users.noreply.github.com>
2025-08-11 11:11:33 -0400

    fix sign in button styling again

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@
 
 import { redirect } from "next/navigation";
 import { useSession } from "next-auth/react";
+import { motion } from "motion/react";
 import { FcGoogle } from "react-icons/fc";
 import { montserrat, roboto } from "@/app/lib/fonts";
 import { signInAction } from "@/app/lib/actions";
@@ -15,10 +16,16 @@ export default function Home() {
       <h1 className={"text-black lg:text-8xl text-7xl font-black " + montserrat.className}>EnTree</h1>
       <p className="text-neutral-700 lg:text-2xl text-xl lg:text-left text-center">Your entry into AI-guided learning.<br></br>Generate a visual learning plan about any topic.</p>
       <form action={signInAction}>
-        <button type="submit" className="border-neutral-500 px-3 py-2 flex items-center gap-3 bg-white border rounded-full hover:cursor-pointer hover:bg-neutral-200 !transition-all !duration-200 !ease-in-out">
+        <motion.button
+          initial={{ backgroundColor: "#ffffff" }}
+          whileHover={{ backgroundColor: "#e5e5e5", boxShadow: "0px 1px 2px 0px #737373" }} // neutral-200, neutral-500
+          transition={{ type: "tween", duration: 0.2, ease: "easeInOut" }}
+          type="submit"
+          className="px-3 py-2 flex items-center gap-3 bg-white border border-neutral-500 rounded-full hover:cursor-pointer !transition-none"
+        >
           <FcGoogle className="size-6" />
           <p className={"text-neutral-700 text-md font-medium " + roboto.className}>Sign in with Google</p>
-        </button>
+        </motion.button>
       </form>
     </div>
   )
